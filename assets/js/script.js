d50ac606b5e683e4dc3bd141397b3b5f07fc400e
boy-mom <100622402+xoerycaxo@users.noreply.github.com>
2022-07-14 14:27:34 -0400

    to fix a problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,29 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// enable draggable/sortable feature on list-group elements
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group")
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    console.log("update", this);
+  }
+});
+
 
 
 
